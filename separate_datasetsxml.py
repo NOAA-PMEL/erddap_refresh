077f3953694d9168bcf85cf0ca1b66bd84335b9c
Ellen Koukel <166429120+e-koukel@users.noreply.github.com>
2025-06-02 11:22:30 -0700

    Create separate_datasetsxml.py

---
+++ separate_datasetsxml.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # purpose: reads in completed datasets.xml and saves individual datasets as xml file fragments
+
+import os
+import xml.etree.ElementTree as ET
+
+#input the entire pathname of your current datasets.xml
+inputFilename = input('Enter a datasets.xml to read in: ')
+
+parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+tree = ET.parse(inputFilename, parser)
+root = tree.getroot()
+
+for elemental in root.iter('dataset'):
+    did = elemental.attrib['datasetID']#finds the datasetID in the <dataset> tag
+    with open('~/erddap_[ERDDAP_NAME]/datasets_xml_fragments/' + did + '.xml','w') as f:#I prefer to add a subdirectory for all the xml fragments so the main directory is cleaner
+        f.write(ET.tostring(elemental, encoding='unicode'))#writes the dataset's <dataset> tag, inclusive, to an xml file named with the datasetID
