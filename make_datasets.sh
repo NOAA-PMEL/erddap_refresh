db82925872e964ddb072df05b07f9c9526b7293e
Ellen Koukel <166429120+e-koukel@users.noreply.github.com>
2026-06-12 10:54:42 -0700

    Create make_datasets.sh

---
+++ make_datasets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#change this to the pathname to your ERDDAP directory
+erddap_path='~/[ERDDAP_NAME]_erddap'
+
+#concatenates first.xml, xinclude_xml.xml (the interior of the datasets.xml), and last.xml into a single datasetsxoncat.xml
+cat $erddap_path/first.xml $erddap_path/datasets_xml_fragments/*.xml $erddap_path/last.xml > $erddap_path/datasetsconcat.xml
+
+#copies datasetsconcat.xml to your real datasets.xml
+cp $erddap_path/datasetsconcat.xml ~/tomcat/content/erddap/datasets.xml
