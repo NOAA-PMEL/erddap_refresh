e89aacae49c49cc0641b4ef1f0e6866cb7dd9432
Ellen Koukel <166429120+e-koukel@users.noreply.github.com>
2026-06-15 10:08:40 -0700

    Update make_datasets.sh

--- make_datasets.sh
+++ make_datasets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #change this to the pathname to your ERDDAP directory
-erddap_path='~/[ERDDAP_NAME]_erddap'
+erddap_path='~/erddap_[ERDDAP_NAME]'
 
 #concatenates first.xml, xinclude_xml.xml (the interior of the datasets.xml), and last.xml into a single datasetsxoncat.xml
 cat $erddap_path/first.xml $erddap_path/datasets_xml_fragments/*.xml $erddap_path/last.xml > $erddap_path/datasetsconcat.xml
